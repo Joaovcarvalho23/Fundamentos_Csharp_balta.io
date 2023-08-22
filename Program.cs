be25027d1b5279b6f9d82f162224097ad3c8a4c2
Joao <joaovictorcordeiro00@gmail.com>
2023-08-22 08:36:40 -0300

    estruturando o código da maneira padrão

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var texto = "Testando";
-Console.WriteLine(texto);
-
 // A importação dos 'namespaces' é feita através do Using!!
 // Essas importações definem quais bibliotecas o nosso programa vai usar;
 // Isso é feito no começo do programa
+
+namespace Fundamentos_Csharp
+{
+    class Program{
+        static void Main (string[] args){
+
+            var texto = "Testando";
+            Console.WriteLine(texto);
+
+        }
+    }
+}
