4428fcf4ea50a4a8147dd5c990735eab00dd1819
Joao <joaovictorcordeiro00@gmail.com>
2023-08-23 09:03:00 -0300

    criando calculadora básica

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace Calculator
+{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            int valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int soma = valor1 + valor2;
+            int soma2 = valor1 + valor2;
+
+            Console.WriteLine("O resultado da soma é {0}", soma);
+            Console.WriteLine($"O resultado da soma é {soma2}");
+        }
+    }    
+}
