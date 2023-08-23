ddb20305f452e91de0cdb79725ce770f3cc2aa83
Joao <joaovictorcordeiro00@gmail.com>
2023-08-23 10:36:15 -0300

    função thread

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,7 +1,21 @@
 ﻿namespace Cronometro{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Start();
         }
+
+        static void Start(){
+            int tempo = 10;
+            int tempoAtual = 0;
+
+            while(tempoAtual != tempo){
+                Console.Clear();
+                tempoAtual++;
+                Console.WriteLine(tempoAtual);
+                Thread.Sleep(1000);
+            }
+        }
+        
     }
 }
