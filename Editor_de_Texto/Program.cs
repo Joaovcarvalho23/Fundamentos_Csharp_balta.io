3900e89f418b0da67f5230080289dad8e46bb924
Joao <joaovictorcordeiro00@gmail.com>
2023-08-23 11:31:45 -0300

    launch.json para o editor de texto

--- Editor_de_Texto/Program.cs
+++ Editor_de_Texto/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Editor_de_Texto
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Clear();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
