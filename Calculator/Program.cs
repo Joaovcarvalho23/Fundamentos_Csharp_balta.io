128848880c88a772eb28bffe23355b0bc6a933a9
Joao <joaovictorcordeiro00@gmail.com>
2023-08-23 09:35:18 -0300

    implementando as 4 operações

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,18 +4,63 @@ namespace Calculator
 {
     class Program{
         static void Main(string[] args){
+            // Soma();
+            // Subtracao();
+           // Divisao();
+            Multiplicacao();
+        }
+
+        static void Soma(){
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
+            Console.WriteLine("\nPrimeiro valor: ");
             int valor1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Segundo valor: ");
             int valor2 = int.Parse(Console.ReadLine());
 
             int soma = valor1 + valor2;
-            int soma2 = valor1 + valor2;
 
-            Console.WriteLine("O resultado da soma é {0}", soma);
-            Console.WriteLine($"O resultado da soma é {soma2}");
+            Console.WriteLine("\nO resultado da soma é {0}", soma);
+            //Console.WriteLine($"O resultado da soma é {soma2}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao(){
+            Console.Clear();
+            Console.WriteLine("\nPrimeiro valor: ");
+            int valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int subtracao = valor1 - valor2;
+
+            Console.WriteLine("\nO resultado da subtração é {0}", subtracao);
+            Console.ReadKey();
+        }
+
+        static void Divisao(){
+            Console.Clear();
+            Console.WriteLine("\nPrimeiro valor: ");
+            var valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            var valor2 = float.Parse(Console.ReadLine());
+
+            float divisao = (float)Math.Round(valor1 / valor2, 2);
+            Console.WriteLine("\nO valor da divisão é {0}", divisao);
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao(){
+            Console.Clear();
+            Console.WriteLine("\nPrimeiro valor: ");
+            var valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            var valor2 = int.Parse(Console.ReadLine());
+
+            int multiplicacao = valor1 * valor2;
+            Console.WriteLine("\nO resultado da multiplicação é {0}", multiplicacao);
+            Console.ReadKey();
         }
     }    
 }
