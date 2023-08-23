e3b904a46a9352a58123866561fd4d8a6f3bd5a0
Joao <joaovictorcordeiro00@gmail.com>
2023-08-23 11:46:09 -0300

    início de implementação do editor

--- Editor_de_Texto/Program.cs
+++ Editor_de_Texto/Program.cs
@@ -3,8 +3,37 @@
     class Program
     {
         static void Main(string[] args){
+            Menu();
+        }
+
+        static void Menu(){
             Console.Clear();
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Selecione a opção desejada abaixo:");
+            Console.WriteLine("1. ABRIR ARQUIVO");
+            Console.WriteLine("2. CRIAR NOVO ARQUIVO");
+            Console.WriteLine("0. SAIR");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch(opcao){
+                case 1: AbrirArquivo(); break;
+                case 2: EditarArquivo(); break;
+                case 0: System.Environment.Exit(0); break;
+                default: ; break;
+            }
+        }
+
+        static void AbrirArquivo(){
+
+        }
+
+        static void EditarArquivo(){
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)\n -----------------------------");
+            string? texto = "";
+
+            while(Console.ReadKey().Key != ConsoleKey.Escape){
+                
+            }
         }
     }
 }
