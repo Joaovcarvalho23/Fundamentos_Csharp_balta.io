ced2d71fabdaa5b0d6e5a2a509e66d8e55fbb549
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 10:13:26 -0300

    manipulando strings

--- Strings/restoDosTopicos/Program.cs
+++ Strings/restoDosTopicos/Program.cs
@@ -20,13 +20,36 @@
             // Console.WriteLine(texto.LastIndexOf("s"));
 
         // ***************** MÉTODOS ADICIONAIS *****************
-            Console.Clear();
-            var texto = "Este texto é um teste";
-            Console.WriteLine(texto.ToUpper());
-            Console.WriteLine(texto.ToLower());
-            Console.WriteLine(texto.Insert(5, "AQUI "));
-            Console.WriteLine(texto.Remove(5, 5));
-            Console.WriteLine(texto.Length);
+            // Console.Clear();
+            // var texto = "Este texto é um teste";
+            // Console.WriteLine(texto.ToUpper());
+            // Console.WriteLine(texto.ToLower());
+            // Console.WriteLine(texto.Insert(5, "AQUI "));
+            // Console.WriteLine(texto.Remove(5, 5));
+            // Console.WriteLine(texto.Length);
+
+        // ***************** MANIPULANDO STRINGS *****************
+            // Console.Clear();
+            // var texto = " Este texto é um teste ";
+            // Console.WriteLine(texto.Replace("Este", "Isso"));
+            // Console.WriteLine(texto.Replace("e", "x"));
+            // Console.WriteLine(" \n");
+
+            // var divisao = texto.Split(" ");
+            // Console.WriteLine(divisao[0]);
+            // Console.WriteLine(divisao[1]);
+            // Console.WriteLine(divisao[2]);
+            // Console.WriteLine(divisao[3]);
+            // Console.WriteLine(" \n");
+
+            // //var resultado = texto.Substring(5, 5);
+            // var resultado = texto.Substring(5, texto.LastIndexOf("o"));
+            // Console.WriteLine(resultado);
+            // Console.WriteLine(" \n");
+
+            // Console.WriteLine(texto.Trim());
+
+
         }
     }
 }
