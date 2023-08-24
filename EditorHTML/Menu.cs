9b4c074c1e249a7354643860d47446159745bffc
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 10:47:10 -0300

    criando o menu

---
+++ EditorHTML/Menu.cs
@@ -0,0 +1,13 @@
+namespace EditorHTML
+{
+    public static class Menu{
+
+        public static void Mostrar(){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue; //cor do fundo
+            Console.ForegroundColor = ConsoleColor.Black; //cor da letra
+
+            
+        }
+    }
+}
