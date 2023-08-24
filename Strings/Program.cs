70fd55f06668be65f5a01908ede5a0835ee70739
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 08:43:47 -0300

    Guids

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.Clear();
+            var id = Guid.NewGuid(); //estamos criando um identificador global único!
+            id.ToString(); //Transformando ele em uma String
+
+            id = new Guid("7567388b-f08a-4d3e-83c6-a050b58b6a62");// podemos passar uma String para ele
+            
+            //pode comparar um guid com outro
+            //if(id == Guid.NewGuid())
+            
+            Console.WriteLine(id.ToString().Substring(0, 8)); //Pega os 8 primeiros caracteres da string
+        }
+    }
+}
