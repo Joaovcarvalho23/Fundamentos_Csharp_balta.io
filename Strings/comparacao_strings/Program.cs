1e00e8e3ad8ce0cf0ab51aaef648242d1d82a913
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 09:23:29 -0300

    métodos para comparar strings

---
+++ Strings/comparacao_strings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace compara_String
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Clear();
+            //var texto = "testando...";
+            // Console.WriteLine(texto.CompareTo("testando..."));
+            // Console.WriteLine(texto.CompareTo("Testando..."));
+            
+            var texto = "Esse texto é um teste";
+            Console.WriteLine(texto.Contains("teste"));
+            Console.WriteLine(texto.Contains("Teste"));
+            Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase)); //"ignore o case sensitive
+        }
+    }
+}
