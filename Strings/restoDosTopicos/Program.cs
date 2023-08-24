0de9eef0c8cb5b55a548a46e9ed8d7ecf3f20c42
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 09:52:05 -0300

    índices

--- Strings/restoDosTopicos/Program.cs
+++ Strings/restoDosTopicos/Program.cs
@@ -5,14 +5,19 @@
         static void Main (string[] args){
             
         // ***************** EQUALS *****************
-            Console.Clear();
-            var texto = "Este texto é um teste";
-            Console.WriteLine(texto.Equals("Este texto é um teste"));
-            Console.WriteLine(texto.Equals("este texto é um teste"));
-            Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
+            // Console.Clear();
+            // var texto = "Este texto é um teste";
+            // Console.WriteLine(texto.Equals("Este texto é um teste"));
+            // Console.WriteLine(texto.Equals("este texto é um teste"));
+            // Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
 
         // ***************** ÍNDICES *****************
-            
+            Console.Clear();
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.IndexOf("é"));
+            Console.WriteLine(texto.IndexOf("um"));
+
+            Console.WriteLine(texto.LastIndexOf("s"));
         }
     }
 }
