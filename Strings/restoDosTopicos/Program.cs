be14b88f9da10cfbab5c6e5a3a592d78e1854113
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 09:57:01 -0300

    métodos adicionais

--- Strings/restoDosTopicos/Program.cs
+++ Strings/restoDosTopicos/Program.cs
@@ -12,12 +12,21 @@
             // Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
 
         // ***************** ÍNDICES *****************
+            // Console.Clear();
+            // var texto = "Este texto é um teste";
+            // Console.WriteLine(texto.IndexOf("é"));
+            // Console.WriteLine(texto.IndexOf("um"));
+
+            // Console.WriteLine(texto.LastIndexOf("s"));
+
+        // ***************** MÉTODOS ADICIONAIS *****************
             Console.Clear();
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.IndexOf("é"));
-            Console.WriteLine(texto.IndexOf("um"));
-
-            Console.WriteLine(texto.LastIndexOf("s"));
+            Console.WriteLine(texto.ToUpper());
+            Console.WriteLine(texto.ToLower());
+            Console.WriteLine(texto.Insert(5, "AQUI "));
+            Console.WriteLine(texto.Remove(5, 5));
+            Console.WriteLine(texto.Length);
         }
     }
 }
