311d2f6f60e85fa81ba15f85452f45fc4c900e4c
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 09:03:09 -0300

    interpolação de strings

--- Strings/Interpolacao_str/Program.cs
+++ Strings/Interpolacao_str/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Interp_Str
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.Clear();
+            var preco = 10.2;
+            //var texto = "O preço do produto é " + preco + " apenas na promoção!";
+            //var texto = string.Format("O preço do produto é {0} apenas na promoção", preco);
+            var texto = $"O preço do produto é {preco} apenas na promoção";
+            Console.WriteLine(texto);
+        }
+    }
+}
