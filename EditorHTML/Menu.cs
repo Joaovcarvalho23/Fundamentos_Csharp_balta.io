1029ac3b13fae9665a7650f8cd01bf67709e649f
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 11:11:08 -0300

    tela tomando corpo

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -7,7 +7,11 @@ namespace EditorHTML
             Console.BackgroundColor = ConsoleColor.Blue; //cor do fundo
             Console.ForegroundColor = ConsoleColor.Black; //cor da letra
 
+            //EscreverOpcoes();
             DesenharTela();
+            EscreverOpcoes();
+
+            var opcao = short.Parse(Console.ReadLine());
         }
 
         public static void DesenharTela(){
@@ -35,7 +39,25 @@ namespace EditorHTML
                 Console.Write("-");
             }
             Console.Write("+");
-            
+
+        }
+    
+        public static void EscreverOpcoes(){
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("===================");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo: ");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3,10);
+            Console.WriteLine("Opção: ");
+            Console.SetCursorPosition(10,10);
         }
     }
 }
