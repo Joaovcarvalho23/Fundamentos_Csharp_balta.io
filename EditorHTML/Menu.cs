d6f7f4c617ba0ad67d68c6c5b282c7a81e00318a
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 10:58:27 -0300

    criando tela básica

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -7,6 +7,34 @@ namespace EditorHTML
             Console.BackgroundColor = ConsoleColor.Blue; //cor do fundo
             Console.ForegroundColor = ConsoleColor.Black; //cor da letra
 
+            DesenharTela();
+        }
+
+        public static void DesenharTela(){
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++){
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for(int j = 0; j <= 10; j++){
+                Console.Write("|");
+
+                for(int i = 0; i <= 30; i++){
+                    Console.Write(" ");
+                }
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++){
+                Console.Write("-");
+            }
+            Console.Write("+");
             
         }
     }
