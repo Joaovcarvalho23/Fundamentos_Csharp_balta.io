0e712da55bd634b5b9ef73cf92e0868acf375f64
Joao <joaovictorcordeiro00@gmail.com>
2023-08-24 09:33:23 -0300

    StartsWith e EndsWith

---
+++ Strings/startsWendsW/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace startsEnd
+{
+    class Program
+    {
+        static void Main (string [] args){
+
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este"));
+            Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(texto.StartsWith("texto"));
+
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.EndsWith("Teste"));
+            Console.WriteLine(texto.EndsWith("xpro"));
+        }
+    }
+}
