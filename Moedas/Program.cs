6a92b8489463284f0e9da3ae4e00d1a5e6123969
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 10:08:19 -0300

    math

--- Moedas/Program.cs
+++ Moedas/Program.cs
@@ -11,10 +11,20 @@ namespace Moedas
             // Console.WriteLine(valor);
 
         // ***************** FORMATANDO MOEDAS *****************
-            decimal valor = 10.25m;
-            Console.WriteLine(valor.ToString(
-                "C",
-                CultureInfo.CreateSpecificCulture("pt-BR")));
-        } //temos o "G", "E", "F"...
+            // decimal valor = 10.25m;
+            // Console.WriteLine(valor.ToString(
+            //     "C",
+            //     CultureInfo.CreateSpecificCulture("pt-BR")));
+
+            // //temos o "G", "E", "F"...
+
+        // ***************** MATH *****************
+            decimal valor = 10536.25m;
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Floor(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+        } 
+
+        
     }
 }
