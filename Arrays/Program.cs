358e7a3c25dc8d503aade84256b8abf6d150afbf
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 10:37:43 -0300

    percorrendo array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -5,20 +5,28 @@
         static void Main(string[] args){
             Console.Clear();
             
-            // var meuArray = new int[5] {1, 23, 15, 27, 75};
-            // meuArray[0] = 12;
-            var meuArray = new Teste[2];
-            meuArray[0] = new Teste();
+            // // var meuArray = new int[5] {1, 23, 15, 27, 75};
+            // // meuArray[0] = 12;
+            // var meuArray = new Teste[2];
+            // meuArray[0] = new Teste();
 
-            Console.WriteLine(meuArray[0].Id);
-            Console.WriteLine(meuArray[1]);
-            Console.WriteLine(meuArray[2]);
-            Console.WriteLine(meuArray[3]);
-            Console.WriteLine(meuArray[4]);
+            // Console.WriteLine(meuArray[0].Id);
+            // Console.WriteLine(meuArray[1]);
+            // Console.WriteLine(meuArray[2]);
+            // Console.WriteLine(meuArray[3]);
+            // Console.WriteLine(meuArray[4]);
+
+        // ******************* PERCORRENDO ARRAY *******************
+            var meuArray = new int[5] {1, 2, 3, 4, 5};
+            meuArray[0] = 12;
+
+            for (var i = 0; i < meuArray.Length; i++){
+                Console.WriteLine(meuArray[i]);
+            }
         }
 
-        struct Teste{
-            public int Id {get; set;}
-        }
+        // struct Teste{
+        //     public int Id {get; set;}
+        // }
     }
 }
