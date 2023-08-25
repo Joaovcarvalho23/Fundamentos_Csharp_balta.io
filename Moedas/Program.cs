f0ec060e9b6f6653b29dce6e3f8e2dd5faa9409d
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 09:50:21 -0300

    Tipo para moedas

--- Moedas/Program.cs
+++ Moedas/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Clear();
+
+            decimal valor = 10.25m;
+            Console.WriteLine(valor);
+        }
+    }
+}
