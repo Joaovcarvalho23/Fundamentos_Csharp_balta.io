ec808a82b2cf8d96b3d2b3b7da187e793784d089
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 08:25:33 -0300

    formatando datas

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -4,19 +4,24 @@
     {
         static void Main(string[] args){
             Console.Clear();
-            
+        // ******************** VALORES DAS DATAS ********************
             //var data = new DateTime(2023, 08, 25, 8, 12, 30);
-            var data = DateTime.Now;
+            // var data = DateTime.Now;
 
-            Console.WriteLine(data);
-            Console.WriteLine(data.Year);
-            Console.WriteLine(data.Month);
-            Console.WriteLine(data.Day);
-            Console.WriteLine(data.Hour);
-            Console.WriteLine(data.Minute);
-            Console.WriteLine(data.Second);
-            Console.WriteLine(data.DayOfWeek);
-            Console.WriteLine(data.DayOfYear);
+            // Console.WriteLine(data);
+            // Console.WriteLine(data.Year);
+            // Console.WriteLine(data.Month);
+            // Console.WriteLine(data.Day);
+            // Console.WriteLine(data.Hour);
+            // Console.WriteLine(data.Minute);
+            // Console.WriteLine(data.Second);
+            // Console.WriteLine(data.DayOfWeek);
+            // Console.WriteLine(data.DayOfYear);
+
+        // ******************** FORMATANDO DATAS ********************
+            var data = DateTime.Now;
+            var formatada = String.Format("{0:dd/MM/yyyy hh:mm:ss}", data);
+            Console.WriteLine(formatada);
         }
     }
 }
