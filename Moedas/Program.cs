7382bed83fd252e8239540933d8696b1eb171666
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 10:01:44 -0300

    formatando moedas

--- Moedas/Program.cs
+++ Moedas/Program.cs
@@ -1,12 +1,20 @@
-﻿namespace Moedas
+﻿using System.Globalization;
+
+namespace Moedas
 {
     class Program
     {
         static void Main(string[] args){
             Console.Clear();
 
+            // decimal valor = 10.25m;
+            // Console.WriteLine(valor);
+
+        // ***************** FORMATANDO MOEDAS *****************
             decimal valor = 10.25m;
-            Console.WriteLine(valor);
-        }
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+        } //temos o "G", "E", "F"...
     }
 }
