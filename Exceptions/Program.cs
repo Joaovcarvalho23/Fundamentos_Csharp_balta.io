78e6048cda2c0a4128f607b8ac7586af2d1b5a7d
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 11:23:16 -0300

    tratamento de erros

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -5,19 +5,46 @@
         static void Main(string[] args){
             Console.Clear();
 
-            var meuArray = new int[3];
+        // ********************* TRY CATCH *********************
+            // var meuArray = new int[3];
 
-            try{
-                for(var i = 0; i < 10; i++){
-                    //System.IndexOutOfRangeException
-                    Console.WriteLine(meuArray[i]);
-                }
-            } catch (Exception ex){
+            // try{
+            //     for(var i = 0; i < 10; i++){
+            //         //System.IndexOutOfRangeException
+            //         Console.WriteLine(meuArray[i]);
+            //     }
+            // } catch (Exception ex){
 
-                Console.WriteLine("Algo deu errado!");
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.InnerException);
-            }
+            //     Console.WriteLine("Algo deu errado!");
+            //     Console.WriteLine(ex.Message);
+            //     Console.WriteLine(ex.InnerException);
+            // }
+
+        // ********************* TRATANDO ERROS *********************]
+             var meuArray = new int[3];
+
+             try{
+                 for(var i = 0; i < 10; i++){
+                     //System.IndexOutOfRangeException
+                     Console.WriteLine(meuArray[i]);
+                 }
+
+                //sempre do mais específico para o mais genérico
+             } catch(IndexOutOfRangeException ex){
+
+                 Console.WriteLine("Não encontrei o índice na lista");
+                 Console.WriteLine(ex.Message);
+                 Console.WriteLine(ex.InnerException);
+
+             } catch (Exception ex){ // esse sempre fica por último!!
+
+                 Console.WriteLine("Algo deu errado!");
+                 Console.WriteLine(ex.Message);
+                 Console.WriteLine(ex.InnerException);
+
+             } 
+
+             
         }
     }
 }
