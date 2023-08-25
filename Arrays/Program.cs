4d91223ebd0631cc3391ed64a82e81ebed3b5057
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 10:27:00 -0300

    iniciando com arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Clear();
+            
+            // var meuArray = new int[5] {1, 23, 15, 27, 75};
+            // meuArray[0] = 12;
+            var meuArray = new Teste[2];
+            meuArray[0] = new Teste();
+
+            Console.WriteLine(meuArray[0].Id);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            Console.WriteLine(meuArray[4]);
+        }
+
+        struct Teste{
+            public int Id {get; set;}
+        }
+    }
+}
