a13eae3830126a96a330ee4bf68b9a1f20a2153b
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 11:16:33 -0300

    usando try catch

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Clear();
+
+            var meuArray = new int[3];
+
+            try{
+                for(var i = 0; i < 10; i++){
+                    //System.IndexOutOfRangeException
+                    Console.WriteLine(meuArray[i]);
+                }
+            } catch (Exception ex){
+
+                Console.WriteLine("Algo deu errado!");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+    }
+}
