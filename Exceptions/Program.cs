bc8590511de68342560a1715b4d2e1c180f35a8a
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 11:31:13 -0300

    disparando exceções

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -21,13 +21,39 @@
             // }
 
         // ********************* TRATANDO ERROS *********************]
-             var meuArray = new int[3];
+            //  var meuArray = new int[3];
+
+            //  try{
+            //      for(var i = 0; i < 10; i++){
+            //          //System.IndexOutOfRangeException
+            //          Console.WriteLine(meuArray[i]);
+            //      }
+
+            //     //sempre do mais específico para o mais genérico
+            //  } catch(IndexOutOfRangeException ex){
+
+            //      Console.WriteLine("Não encontrei o índice na lista");
+            //      Console.WriteLine(ex.Message);
+            //      Console.WriteLine(ex.InnerException);
+
+            //  } catch (Exception ex){ // esse sempre fica por último!!
+
+            //      Console.WriteLine("Algo deu errado!");
+            //      Console.WriteLine(ex.Message);
+            //      Console.WriteLine(ex.InnerException);
+
+            //  } 
+
+        // ********************* DISPARANDO EXCESSÕES *********************]
+            var meuArray = new int[3];
 
              try{
-                 for(var i = 0; i < 10; i++){
-                     //System.IndexOutOfRangeException
-                     Console.WriteLine(meuArray[i]);
-                 }
+                //  for(var i = 0; i < 10; i++){
+                //      //System.IndexOutOfRangeException
+                //      Console.WriteLine(meuArray[i]);
+                //  }
+
+                 Salvar("");
 
                 //sempre do mais específico para o mais genérico
              } catch(IndexOutOfRangeException ex){
@@ -36,15 +62,29 @@
                  Console.WriteLine(ex.Message);
                  Console.WriteLine(ex.InnerException);
 
-             } catch (Exception ex){ // esse sempre fica por último!!
+             } catch (ArgumentNullException ex){ // esse sempre fica por último!!
+
+                 Console.WriteLine("Falha ao cadastrar texto");
+                 Console.WriteLine(ex.Message);
+                 Console.WriteLine(ex.InnerException);
+
+             }  
+             
+             catch (Exception ex){ // esse sempre fica por último!!
 
                  Console.WriteLine("Algo deu errado!");
                  Console.WriteLine(ex.Message);
                  Console.WriteLine(ex.InnerException);
 
              } 
+        }
 
-             
+        private static void Salvar(string texto){
+
+            if(string.IsNullOrEmpty(texto)){
+                //throw new Exception("O texto não pode ser nulo ou vazio!");
+                throw new ArgumentNullException("O texto não pode ser nulo ou vazio!");
+            }
         }
     }
 }
