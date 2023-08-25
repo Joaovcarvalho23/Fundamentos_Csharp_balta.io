6f64eec8f54a3442d44ee3e70ac01a46a7fd2a78
Joao <joaovictorcordeiro00@gmail.com>
2023-08-25 08:16:49 -0300

    DateTime.Tipos

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -5,10 +5,18 @@
         static void Main(string[] args){
             Console.Clear();
             
-            //var data = new DateTime();
-
+            //var data = new DateTime(2023, 08, 25, 8, 12, 30);
             var data = DateTime.Now;
+
             Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
+            Console.WriteLine(data.Hour);
+            Console.WriteLine(data.Minute);
+            Console.WriteLine(data.Second);
+            Console.WriteLine(data.DayOfWeek);
+            Console.WriteLine(data.DayOfYear);
         }
     }
 }
